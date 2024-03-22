58cf51ea1fd78db58c4f2b723153701d3cc0aba7
biyam <101965666+biyamn@users.noreply.github.com>
2024-03-22 07:07:11 +0000

    feat: step5 헤더, 푸터 추가

--- workshop/MyPortfolio/Models/SocialIcons.cs
+++ workshop/MyPortfolio/Models/SocialIcons.cs
@@ -1,9 +1,7 @@
 namespace MyPortfolio.Models;
 
-public class SiteProperties
+public class SocialIcons
 {
-    public string Name { get; set; } = string.Empty;
-    public string Title { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string LinkedIn { get; set; } = string.Empty;
     public string GitHub { get; set; } = string.Empty;
